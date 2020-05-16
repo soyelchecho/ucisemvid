ad02140eaeacbcb967995947fad5a5b371f8c68e
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-05-15 20:10:04 -0500

    Updating Gato Prefab

--- VR-Multiplayer-Game/Assets/Scripts/Networking/AnimalSpawn.cs
+++ VR-Multiplayer-Game/Assets/Scripts/Networking/AnimalSpawn.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class AnimalSpawn : MonoBehaviour
+public class AnimalSpawn : MonoBehaviourPunCallbacks, Photon.Pun.IPunObservable
 {
 
     public Vector3 initialPoint;
     public GameObject gato;
+    public int cantidad;
 
     // Start is called before the first frame update
     void Start()
     {
+        cantidad = 0;
         initialPoint = new Vector3(0, 10f, 0);
         StartCoroutine(Spawn(10, initialPoint));
     }
@@ -21,22 +26,32 @@ public class AnimalSpawn : MonoBehaviour
         
     }
 
-    private IEnumerator Spawn(float cantidad, Vector3 initialPosition) {
-        float randX = Random.Range(-10.0f, 10.0f);
-        float randZ = Random.Range(-10.0f, 10.0f);
+    private IEnumerator Spawn(float needed, Vector3 initialPosition) {
+        float randX = Random.Range(-20.0f, 20.0f);
+        float randZ = Random.Range(-20.0f, 20.0f);
         Vector3 startPos = initialPosition;
         startPos += new Vector3(randX, randZ);
 
-        while (cantidad != 0) {
-            GameObject animal = Instantiate(gato, startPos, Quaternion.identity);
+        while (needed != 0) {
+            GameObject animal = PhotonNetwork.Instantiate("Gato Prefab", startPos, Quaternion.identity, 0);
             animal.transform.rotation = Quaternion.LookRotation(startPos);
-            randX = Random.Range(-10.0f, 10.0f);
-            randZ = Random.Range(-10.0f, 10.0f);
+            randX = Random.Range(-20.0f, 20.0f);
+            randZ = Random.Range(-20.0f, 20.0f);
             startPos += new Vector3(randX, randZ);
-            cantidad -= 1;
+            needed -= 1;
+            cantidad += 1;
             yield return null;
         }
 
         yield return new WaitForSeconds(1.0f);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting) {
+            stream.SendNext(cantidad);
+        } else {
+            cantidad = (int)stream.ReceiveNext();   
+        }
     }
 }
